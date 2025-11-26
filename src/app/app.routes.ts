a7d3ccbd957bd240e5f8ab51170724bbd7752b54
Pedro Arthur <pedro.arthurnery11@gmail.com>
2025-11-26 02:12:23 -0300

    criação de modais, cards de adicionar comanda e ajustes na estilização da home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home';
+import { ProductComponent } from './components/product/product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'produtos', component: ProductComponent },
+  { path: 'relatorio', component: HomeComponent }, 
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
