082d6524518ff35b47238c39c461a20071cb2f45
Pedro Arthur <pedro.arthurnery11@gmail.com>
2025-11-27 22:35:49 -0300

    Tirando a navegação produtos do header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
-import { ProductComponent } from './components/product/product';
+
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'produtos', component: ProductComponent },
   { path: 'relatorio', component: HomeComponent }, 
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
